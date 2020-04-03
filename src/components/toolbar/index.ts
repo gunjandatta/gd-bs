df947c8af5fc7e2818549784eceb3ac8765bfd1e
Gunjan Datta <me@dattabase.com>
2020-04-03 11:46:28 -0400

    Converted the toolbar component.

--- src/components/toolbar/index.ts
+++ src/components/toolbar/index.ts
@@ -1,8 +1,7 @@
 import { IToolbar, IToolbarProps } from "../../../@types/components/toolbar";
-import * as Common from "../common";
+import { Base } from "../base";
 import { ButtonGroup } from "../buttonGroup";
 import { InputGroup } from "../inputGroup";
-import { Base } from "../base";
 import * as HTML from "./index.html";
 
 /**
@@ -22,69 +21,36 @@ class _Toolbar extends Base<IToolbarProps> implements IToolbar {
 
     // Configure the card group
     private configure() {
+        // Parse the items
+        let items = this.props.items || [];
+        for (let i = 0; i < items.length; i++) {
+            let item = items[i];
+            let el = null;
+
+            // See if this is a button group
+            if (item.buttons) {
+                // Render an button group
+                el = ButtonGroup({ buttons: item.buttons }).el;
+            }
+
+            // See if this is an input group
+            if (item.inputGroup) {
+                // Render an input group
+                el = InputGroup(item.inputGroup).el;
+            }
+
+            // Ensure the element exists
+            if (el) {
+                // See if there is a spacing value defined, and this is not the last element
+                if (this.props.spacing > 0 && i < items.length - 1) {
+                    // Add the spacing
+                    el.classList.add("mr-" + this.props.spacing);
+                }
+
+                // Append the element
+                this.el.appendChild(el);
+            }
+        }
     }
 }
-export const Toolbar = (props: IToolbarProps): IToolbar => {
-    // Create the toolbar
-    let toolbar = document.createElement("div") as HTMLDivElement;
-
-    // Set the properties
-    props.className ? toolbar.className = props.className : null;
-    toolbar.classList.add("btn-toolbar");
-    toolbar.setAttribute("role", "toolbar");
-
-    // Parse the items
-    let items = props.items || [];
-    for (let i = 0; i < items.length; i++) {
-        let item = items[i];
-
-        // See if this is a button group
-        if (item.buttons) {
-            // Render an button group
-            toolbar.appendChild(ButtonGroup({ buttons: item.buttons }).el);
-        }
-
-        // See if this is an input group
-        if (item.inputGroup) {
-            // Render an input group
-            toolbar.appendChild(InputGroup(item.inputGroup).el);
-        }
-
-        // See if there is a spacing value defined, and this is not the last element
-        if (props.spacing > 0 && i < items.length - 1) {
-            // Add the spacing
-            toolbar.children[i].classList.add("mr-" + props.spacing);
-        }
-    }
-
-    // Create the element
-    let el = document.createElement("div");
-    el.appendChild(toolbar);
-
-    // See if we are rendering it to an element
-    if (props.el) {
-        // Ensure the class list exists and it's not the body element
-        if (props.el.classList && props.el.tagName != "BODY") {
-            // Set the bootstrap class
-            props.el.classList.contains("bs") ? null : props.el.classList.add("bs");
-        }
-
-        // Append the elements
-        while (el.children.length > 0) {
-            props.el.appendChild(el.children[0]);
-        }
-
-        // Update the element
-        el = props.el as any;
-    } else {
-        // Set the bootstrap class
-        el.classList.add("bs");
-    }
-
-    // Return the toolbar
-    return {
-        el,
-        hide: () => { Common.hide(el); },
-        show: () => { Common.show(el); }
-    };
-}
+export const Toolbar = (props: IToolbarProps): IToolbar => { return new _Toolbar(props); }
