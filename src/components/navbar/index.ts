f19adb163db75727a43f0b00e094009791fd8ec4
Gunjan Datta <me@dattabase.com>
2020-04-14 12:52:27 -0400

    Bug Fix - The "click" event wasn't triggered for a search input element. We have to use "mouseup" for the Edge browser bug.

--- src/components/navbar/index.ts
+++ src/components/navbar/index.ts
@@ -129,8 +129,8 @@ class _Navbar extends Base<INavbarProps> implements INavbar {
                 // Edge has a bug where the clear event isn't triggered
                 // See if this is the Edge browser
                 if (window.navigator.userAgent.indexOf("Edge") > 0) {
-                    // Add a click event
-                    searchbox.addEventListener("click", () => {
+                    // Detect the mouse click event
+                    searchbox.addEventListener("mouseup", () => {
                         let currentValue = searchbox.value;
 
                         // Set a timeout to see if the value is cleared
