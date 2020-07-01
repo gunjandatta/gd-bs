d487962216b38621e11b3d2e63fb4aa011055f21
Gunjan Datta <me@dattabase.com>
2020-07-01 13:58:09 -0400

    Added the container class to the navbar. v5 requires the navbar to have a container class.

--- src/components/navbar/templates.ts
+++ src/components/navbar/templates.ts
@@ -1,18 +1,20 @@
 // Navbar
 export const HTML = `
 <nav class="navbar navbar-expand-lg">
-    <a class="navbar-brand" href="#"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false"
-        aria-label="Toggle Navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse">
-        <ul class="navbar-nav mr-auto mb-2 mb-lg-0"></ul>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false"
+            aria-label="Toggle Navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto mb-2 mb-lg-0"></ul>
+        </div>
+        <form class="d-flex">
+            <input class="form-control mr-2" type="search" placeholder="Search" aria-label="Search" />
+            <button class="btn" type="submit">Search</button>
+        </form>
     </div>
-    <form class="d-flex">
-        <input class="form-control mr-2" type="search" placeholder="Search" aria-label="Search" />
-        <button class="btn" type="submit">Search</button>
-    </form>
 </nav>`.trim();
 
 // Item
