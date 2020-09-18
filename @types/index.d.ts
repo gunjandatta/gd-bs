92cc0b3e316648283e0960e897e5c18fadd44363
Gunjan Datta <me@dattabase.com>
2020-09-18 10:26:25 -0400

    Updated the intellisense to include the bootstrap lib.

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,7 +1,9 @@
 import * as Components from "./components";
 import { Icons, IconTypes } from "./icons";
+declare var bootstrap: any;
 
 export {
+    bootstrap,
     Components,
     Icons, IconTypes
 }
