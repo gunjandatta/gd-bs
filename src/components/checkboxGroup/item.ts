7bbbe16e2ecd9a865be264eedd34d34778e49900
Gunjan Datta <me@dattabase.com>
2020-10-19 12:41:41 -0400

    Updated the checkbox to target the input element, not parent div for setting the "title" property.

--- src/components/checkboxGroup/item.ts
+++ src/components/checkboxGroup/item.ts
@@ -36,10 +36,16 @@ export class CheckboxItem {
         this._elCheckbox = this._el.querySelector("input");
         if (this._elCheckbox) {
             this._elCheckbox.disabled = this._parent.isReadonly || this._props.isDisabled ? true : false;
+
+            // Default the title property for the checkbox
+            this._elCheckbox.title = this._parent.title || this.props.label || "";
         }
 
-        // Set the title
-        this._el.title = this._parent.title || this.props.label || "";
+        // See if the title property is defined
+        if (this._parent.title) {
+            // Set the title
+            this._el.title = this._parent.title;
+        }
 
         // See if the inline flag is set
         if (this._parent.isInline) {
