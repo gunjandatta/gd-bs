fecc7ca6653967f673e0b1a82ba00d19d1c26673
Gunjan Datta <me@dattabase.com>
2020-12-09 21:53:50 -0500

    Removed the custom event from the collapse.

--- src/components/collapse/index.ts
+++ src/components/collapse/index.ts
@@ -14,9 +14,6 @@ class _Collapse extends Base<ICollapseProps> implements ICollapse {
         // Configure the collapse
         this.configure();
 
-        // Configure the events
-        this.configureEvents();
-
         // Configure the parent
         this.configureParent();
     }
@@ -43,36 +40,6 @@ class _Collapse extends Base<ICollapseProps> implements ICollapse {
 
         // Execute the render event
         this.props.onRender ? this.props.onRender(this.props, body) : null;
-    }
-
-    // Configures the events	
-    // The collapse JS isn't working. This is a fix until the core is fixed.	
-    private configureEvents() {
-        let isVisible = this.el.classList.contains("show");
-
-        // Create a custom event for watching the style attribute change	
-        let watchStyleChanges = new MutationObserver(mutations => {
-            // Wait for the events to complete	
-            if (this.el.classList.contains("collapsing")) { return; }
-
-            // See if the element is visible	
-            if (isVisible) {
-                // Add the collapsing class for animation	
-                this.el.classList.add("collapsing");
-                setTimeout(() => {
-                    // Remove the show class	
-                    this.el.classList.remove("show");
-                    this.el.classList.remove("collapsing");
-                }, 250);
-            } else {
-                // Remove the show class	
-                this.el.classList.add("show");
-            }
-
-            // Update the flag	
-            isVisible = !isVisible;
-        });
-        watchStyleChanges.observe(this.el, { attributes: true, attributeFilter: ["style"] });
     }
 
     /**
