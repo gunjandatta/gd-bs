3cfcf2b62bd43824ad4865d033386a323bc45fe2
Gunjan Datta <me@dattabase.com>
2020-12-13 22:50:15 -0500

    Updated the toast.

--- src/components/toast/templates.ts
+++ src/components/toast/templates.ts
@@ -2,7 +2,7 @@ export const HTML = `
 <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
     <div class="toast-header">
         <img class="rounded mr-2">
-        <strong class="mr-auto"></strong>
+        <strong class="me-auto"></strong>
         <small></small>
         <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
     </div>
