7cc07d6e4626bc3be2a280148f61a3f8f65d77f1
Gunjan Datta <me@dattabase.com>
2021-06-06 14:55:37 -0400

    Updated the intellisense.

--- dist/gd-bs.d.ts
+++ dist/gd-bs.d.ts
@@ -2035,6 +2035,12 @@ declare module 'gd-bs/components/offcanvas' {
             /** Hides a collapsible element. */
             hide: () => void;
     
+            /** Returns true if the modal is visible. */
+            isVisible: boolean;
+    
+            /** Updates the type. */
+            setType: (canvasType: number) => void;
+    
             /** Shows a collapsible element. */
             show: () => void;
     
