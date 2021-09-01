ad98e7e5baed555bfac281a6f5735c18fe492c61
Gunjan Datta <me@dattabase.com>
2021-09-01 17:33:09 -0400

    Updated the styling import. Some of the files do not need to be under the .bs class.

--- webpack.sass.js
+++ webpack.sass.js
@@ -42,6 +42,22 @@ module.exports = (env, argv) => {
                     { loader: "style-loader" },
                     // Translate CSS to CommonJS
                     { loader: "css-loader" },
+                    // Run postcss actions
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            // `postcssOptions` is needed for postcss 8.x;
+                            // if you use postcss 7.x skip the key
+                            postcssOptions: {
+                                // postcss plugins, can be exported to postcss.config.js
+                                plugins: function() {
+                                    return [
+                                        require('autoprefixer')
+                                    ];
+                                }
+                            }
+                        }
+                    },
                     // Compile SASS to CSS
                     { loader: "sass-loader" }
                 ]
