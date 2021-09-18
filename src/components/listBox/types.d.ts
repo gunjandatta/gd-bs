3a79fa71ed48ce6d6da88420a12d9d024e39ba98
Gunjan Datta <me@dattabase.com>
2021-09-18 12:05:33 -0400

    Added code example.

--- src/components/listBox/types.d.ts
+++ src/components/listBox/types.d.ts
@@ -1,5 +1,72 @@
 /**
+ * <div id="demo"></div>
+ * <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gd-sprest-bs/5.0.3/gd-sprest-bs.min.js"></script>
+ * <script type="text/javascript">
+ *     // Wait for the window to be loaded
+ *     window.addEventListener("load", function() {
+ *         // Create the list box
+ *         $REST.Components.ListBox({
+ *             el: document.querySelector("#demo"),
+ *             label: "Colors",
+ *             placeholder: "Search Colors",
+ *             items: [
+ *                  {
+ *                      text: "Red",
+ *                      value: "red"
+ *                  },
+ *                  {
+ *                      text: "Blue",
+ *                      value: "blue"
+ *                  },
+ *                  {
+ *                      text: "Green",
+ *                      value: "green"
+ *                  },
+ *                  {
+ *                      text: "Purple",
+ *                      value: "purple"
+ *                  },
+ *                  {
+ *                      text: "Brown",
+ *                      value: "brown"
+ *                  },
+ *                  {
+ *                      text: "Yellow",
+ *                      value: "yellow"
+ *                  },
+ *                  {
+ *                      text: "Orange",
+ *                      value: "orange"
+ *                  }
+ *             ]
+ *         });
+ *     });
+ * </script>
+ */
+
+/**
  * List Box
+ * 
+ * ```ts
+ * import { Components } from "gd-sprest-bs";
+ * 
+ * // Create the list box
+ * let el = document.querySelector("#list-box");
+ * Components.Collapse({
+ *     el: el,
+ *     label: "Colors",
+ *     placeholder: "Search Colors",
+ *     items: [
+ *          { text: "Red", value: "red" },
+ *          { text: "Blue", value: "blue" },
+ *          { text: "Green", value: "green" },
+ *          { text: "Purple", value: "purple" },
+ *          { text: "Brown", value: "brown" },
+ *          { text: "Yellow", value: "yellow" },
+ *          { text: "Orange", value: "orange" }
+ *     ]
+ * });
+ * ```
  */
 export const ListBox: (props: IListBoxProps, template?: string) => IListBox;
 
