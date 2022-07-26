ad1c7bbb7ad995751355e7d261d94025e790572f
Gunjan Datta <me@dattabase.com>
2022-07-26 19:21:41 -0400

    Fixed the export of tippy.

--- src/tippy.js/index.ts
+++ src/tippy.js/index.ts
@@ -1,14 +1,15 @@
-const tippy = require("./tippy.js");
+const tippyJS = require("./tippy.js");
 
 // Plugins
-export const animateFill = tippy.animateFill;
-export const createSingleton = tippy.createSingleton;
-export const delegate = tippy.delegate;
-export const followCursor = tippy.followCursor;
-export const hideAll = tippy.hideAll;
-export const inlinePositioning = tippy.inlinePositioning;
-export const roundArrow = tippy.roundArrow;
-export const sticky = tippy.sticky;
+export const animateFill = tippyJS.animateFill;
+export const createSingleton = tippyJS.createSingleton;
+export const delegate = tippyJS.delegate;
+export const followCursor = tippyJS.followCursor;
+export const hideAll = tippyJS.hideAll;
+export const inlinePositioning = tippyJS.inlinePositioning;
+export const roundArrow = tippyJS.roundArrow;
+export const sticky = tippyJS.sticky;
 
 // Default
+const tippy = tippyJS.default || tippyJS;
 export default tippy;
