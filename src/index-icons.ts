1fad2d7b5598bb4aea10f0aeefef68b7f5e8e73f
Gunjan Datta <me@dattabase.com>
2022-07-29 11:46:24 -0400

    Updated the sass and removed the js only version.

--- src/index-icons.ts
+++ src/index-icons.ts
@@ -1,13 +1,21 @@
 // Bootstrap styles
 import "./bs";
 
-// Export the core
-export * from "./index-core";
+// Import the IE fix
+import "./ie";
+
+// Bootstrap Components
+import * as Components from "./components";
+export { Components }
+
+// TippyJS library
+import tippy from "tippy.js";
+export { tippy }
 
 // Icons
 import { Icons, IconTypes } from "./icons";
 export { Icons, IconTypes }
 
-// Include the Icons in the Global library
-window["GD"].Icons = Icons;
-window["GD"].IconTypes = IconTypes;
+// Bootstrap Global library
+const BS = { Components, Icons, IconTypes, tippy }
+window["GD"] = window["GD"] || BS;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 // Bootstrap styles
 import "./bs";
 
-// Export the core
-export * from "./index-core";
+// Import the IE fix
+import "./ie";
+
+// Bootstrap Components
+import * as Components from "./components";
+export { Components }
+
+// TippyJS library
+import tippy from "tippy.js";
+export { tippy }
+
+// Bootstrap Global library
+const BS = { Components, tippy }
+window["GD"] = window["GD"] || BS;
