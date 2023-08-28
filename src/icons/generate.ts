37608e30cd9317f1198d38479986588d8eb98dd7
Gunjan Datta <me@dattabase.com>
2023-08-28 14:29:47 -0400

    Make the icon hidden for the screen reader.

--- src/icons/generate.ts
+++ src/icons/generate.ts
@@ -5,25 +5,28 @@ export const generateIcon = (svg: string, height: number = 32, width: number = 3
 	elDiv.innerHTML = svg;
 	let icon = elDiv.firstChild as HTMLElement;
 	if (icon) {
-	    // See if a class name exists
-	    if (className) {
-	      // Parse the class names
-	      let classNames = className.split(' ');
-	      for (let i = 0; i < classNames.length; i++) {
-	        // Add the class name
-	        icon.classList.add(classNames[i]);
-	      }
-	    }
+		// See if a class name exists
+		if (className) {
+			// Parse the class names
+			let classNames = className.split(' ');
+			for (let i = 0; i < classNames.length; i++) {
+				// Add the class name
+				icon.classList.add(classNames[i]);
+			}
+		}
 
-	    // Set the height/width
-	    icon.setAttribute("height", (height ? height : 32).toString());
-	    icon.setAttribute("width", (width ? width : 32).toString());
+		// Make this icon invisible to the screen reader
+		icon.setAttribute("aria-hidden", "true");
 
-	    // Update the styling
-	    icon.style.pointerEvents = "none";
+		// Set the height/width
+		icon.setAttribute("height", (height ? height : 32).toString());
+		icon.setAttribute("width", (width ? width : 32).toString());
 
-	    // Support for IE
-	    icon.setAttribute("focusable", "false");
+		// Update the styling
+		icon.style.pointerEvents = "none";
+
+		// Support for IE
+		icon.setAttribute("focusable", "false");
 	}
 
 	// Return the icon
