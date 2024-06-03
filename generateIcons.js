902da1db553199c52de59e4266f87123ec35d56d
Gunjan Datta <me@dattabase.com>
2024-06-03 09:27:48 -0400

    Updated the generate icon method.

--- generateIcons.js
+++ generateIcons.js
@@ -140,7 +140,7 @@ fs.readdir(dirIcons, function (err, files) {
         '\tif (placeholders > 0) {',
         '\t\t// Parse the number of placeholders',
         '\t\tfor (let i = 0; i < placeholders; i++) {',
-        '\t\t\tlet regex = new RegExp("/\\{" + i + "\\}/g");\n',
+        '\t\t\tlet regex = new RegExp("\\{" + i + "\\}", "g");\n',
         '\t\t\t// Replace the placeholder',
         '\t\t\tsvg = svg.replace(regex, Date.now().toString());',
         '\t\t}',
