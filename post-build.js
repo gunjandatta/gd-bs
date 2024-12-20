75373333ec500f6db4eb674b9f8bba3a86026af4
Gunjan Datta <me@dattabase.com>
2024-12-20 13:08:52 -0500

    Updated the build for icons to include the .d.ts in the build dir.

--- post-build.js
+++ post-build.js
@@ -2,6 +2,9 @@ var fs = require('fs');
 
 // Copy the type definition file
 fs.copyFileSync("src/icons/svgs/index.d.ts", "build/icons/svgs/index.d.ts");
+fs.readdirSync("src/icons/svgs").filter(fn => fn.endsWith(".d.ts")).forEach(filename => {
+    fs.copyFileSync(`src/icons/svgs/${filename}`, `build/icons/svgs/${filename}`);
+});
 
 // Log
 console.log("Successfully copied the icons definition");
