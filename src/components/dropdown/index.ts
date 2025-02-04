dbc63094345da0a778b38cb0d3ebde8c109e41ec
Gunjan Datta <me@dattabase.com>
2025-02-04 14:03:58 -0500

    Fixed logic from last check-in.

--- src/components/dropdown/index.ts
+++ src/components/dropdown/index.ts
@@ -538,7 +538,7 @@ class _Dropdown extends Base<IDropdownProps> implements IDropdown {
                         // Parse the items and ensure none are toggled except for the selected index
                         for (let i = 0; i < this._items.length; i++) {
                             // Ensure it's not selected
-                            if (this._items[i].isSelected) { this._items[i].toggle(); }
+                            if (i != idx && this._items[i].isSelected) { this._items[i].toggle(); }
                             // Else, toggle it if it's the selected item
                             else if (idx == i && this._items[i].isSelected == false) { this._items[i].toggle(); }
                         }
