59da948a7ec20d7431b8bec3bf27884565ee5405
Gunjan Datta <me@dattabase.com>
2025-07-07 14:41:36 -0400

    Updated the custom elements js to include a check to ensure the element hasn't been defined already.

--- src/custom-elements.js
+++ src/custom-elements.js
@@ -13,34 +13,44 @@ class BSElement extends HTMLElement {
         }
     }
 }
-customElements.define("bs-accordion", class Accordion extends BSElement { });
-customElements.define("bs-alert", class Alert extends BSElement { });
-customElements.define("bs-badge", class Badge extends BSElement { });
-customElements.define("bs-breadcrumb", class Breadcrumb extends BSElement { });
-customElements.define("bs-button", class Button extends BSElement { });
-customElements.define("bs-button-group", class ButtonGroup extends BSElement { });
-customElements.define("bs-collapse", class Collapse extends BSElement { });
-customElements.define("bs-card", class Card extends BSElement { });
-customElements.define("bs-card-group", class CardGroup extends BSElement { });
-customElements.define("bs-carousel", class Carousel extends BSElement { });
-customElements.define("bs-checkbox-group", class CheckboxGroup extends BSElement { });
-customElements.define("bs-dropdown", class Dropdown extends BSElement { });
-customElements.define("bs-form", class Form extends BSElement { });
-customElements.define("bs-form-control", class FormControl extends BSElement { });
-customElements.define("bs-icon-link", class IconLink extends BSElement { });
-customElements.define("bs-input-group", class InputGroup extends BSElement { });
-customElements.define("bs-list-box", class ListBox extends BSElement { });
-customElements.define("bs-list-group", class ListGroup extends BSElement { });
-customElements.define("bs-modal", class Modal extends BSElement { });
-customElements.define("bs-nav", class Nav extends BSElement { });
-customElements.define("bs-navbar", class Navbar extends BSElement { });
-customElements.define("bs-offcanvas", class Offcanvas extends BSElement { });
-customElements.define("bs-paging", class Paging extends BSElement { });
-customElements.define("bs-popover", class Popover extends BSElement { });
-customElements.define("bs-progress", class Progress extends BSElement { });
-customElements.define("bs-progress-group", class ProgressGroup extends BSElement { });
-customElements.define("bs-spinner", class Spinner extends BSElement { });
-customElements.define("bs-table", class Table extends BSElement { });
-customElements.define("bs-toast", class Toast extends BSElement { });
-customElements.define("bs-toolbar", class Toolbar extends BSElement { });
-customElements.define("bs-tooltip", class Tooltip extends BSElement { });
+
+// Method to check if the element doesn't exist and create the custom element
+function defineElement(bsName, bsElement) {
+    // Ensure it hasn't been created
+    if (customElements.get(bsName)) { return; }
+
+    // Define the custom element
+    customElements.define(bsName, bsElement);
+}
+
+defineElement("bs-accordion", class Accordion extends BSElement { });
+defineElement("bs-alert", class Alert extends BSElement { });
+defineElement("bs-badge", class Badge extends BSElement { });
+defineElement("bs-breadcrumb", class Breadcrumb extends BSElement { });
+defineElement("bs-button", class Button extends BSElement { });
+defineElement("bs-button-group", class ButtonGroup extends BSElement { });
+defineElement("bs-collapse", class Collapse extends BSElement { });
+defineElement("bs-card", class Card extends BSElement { });
+defineElement("bs-card-group", class CardGroup extends BSElement { });
+defineElement("bs-carousel", class Carousel extends BSElement { });
+defineElement("bs-checkbox-group", class CheckboxGroup extends BSElement { });
+defineElement("bs-dropdown", class Dropdown extends BSElement { });
+defineElement("bs-form", class Form extends BSElement { });
+defineElement("bs-form-control", class FormControl extends BSElement { });
+defineElement("bs-icon-link", class IconLink extends BSElement { });
+defineElement("bs-input-group", class InputGroup extends BSElement { });
+defineElement("bs-list-box", class ListBox extends BSElement { });
+defineElement("bs-list-group", class ListGroup extends BSElement { });
+defineElement("bs-modal", class Modal extends BSElement { });
+defineElement("bs-nav", class Nav extends BSElement { });
+defineElement("bs-navbar", class Navbar extends BSElement { });
+defineElement("bs-offcanvas", class Offcanvas extends BSElement { });
+defineElement("bs-paging", class Paging extends BSElement { });
+defineElement("bs-popover", class Popover extends BSElement { });
+defineElement("bs-progress", class Progress extends BSElement { });
+defineElement("bs-progress-group", class ProgressGroup extends BSElement { });
+defineElement("bs-spinner", class Spinner extends BSElement { });
+defineElement("bs-table", class Table extends BSElement { });
+defineElement("bs-toast", class Toast extends BSElement { });
+defineElement("bs-toolbar", class Toolbar extends BSElement { });
+defineElement("bs-tooltip", class Tooltip extends BSElement { });
